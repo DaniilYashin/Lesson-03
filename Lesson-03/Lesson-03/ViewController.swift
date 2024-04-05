4c8594ec07b050cc95d241c273e4ce3e159ba375
DaniilYashin <dyashin07@gmail.com>
2024-04-05 20:33:04 +0300

    created new project for lesson-03

---
+++ Lesson-03/Lesson-03/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson-03
+//
+//  Created by Даниил Яшин on 5.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
